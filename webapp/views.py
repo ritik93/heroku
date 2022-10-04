94e7d71b91b54334e5e36675853a9813d33dfd5d
Ritik Chhabra <ritikchhabra93@gmail.com>
2022-10-04 23:43:33 +0100

    Adding project files

---
+++ webapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+import datetime
+
+
+TEMPLATE_DIRS = (
+    'os.path.join(BASE_DIR, "templates")'
+)
+
+
+def index(request):
+    today = datetime.datetime.now().date()
+    return render(request, "index.html", {"today": today})
